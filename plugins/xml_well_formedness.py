fa79c8a506ca49b27aced26311c53bea9839c5c1
vignesh <me@vikki.in>
2015-10-12 18:40:14 +0530

    Added validation of file existense before the formedness check

--- plugins/xml_well_formedness.py
+++ plugins/xml_well_formedness.py
@@ -1,16 +1,29 @@
 import xml.parsers.expat,sys
 from glob import glob
+import os
+import os.path
+import sys
+
 
 def parsefile(file):
     parser = xml.parsers.expat.ParserCreate()
     parser.ParseFile(open(file, "r"))
 
-for arg in sys.argv[1:]:
-    for filename in glob(arg):
-        try:
-            parsefile(filename)
-            print "%s is well-formed" % filename
+def xml_check():
+    for arg in sys.argv[1:]:
+        for filename in glob(arg):
+            try:
+                parsefile(filename)
+                print "%s is well-formed" % filename
 			#sys.exit(0)
-        except Exception, e:
-            print "%s is %s" % (filename, e)
+            except Exception, e:
+                print "%s is %s" % (filename, e)
 			#sys.exit(2)
+
+LOG_FILE = sys.argv[1]
+if os.path.isfile(LOG_FILE):
+   xml_check()
+else:
+   print LOG_FILE ,"- does not exist"
+   #sys.exit(2)
+
